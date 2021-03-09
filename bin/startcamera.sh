74d6f87d859c472a01387a5248484aa920ab7e00
beardage <xypress@gmail.com>
2021-03-08 22:15:35 -0700

    fixing kill command awk argument

--- bin/startcamera.sh
+++ bin/startcamera.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-kill $(ps aux | grep 'gvfs-gphoto2-volume-monitor' | awk '$2')
-kill $(ps aux | grep 'gvfsd-gphoto2 --spawner' | awk '$2')
+kill -9 $(ps aux | grep 'gvfs-gphoto2-volume-monitor' | awk '{print $2}')
+kill -9 $(ps aux | grep 'gvfsd-gphoto2 --spawner' | awk '{print $2}')
 
-echo gphoto2 --auto-detect
+gphoto2 --auto-detect
+
 echo 'camera ready for capture';
 
 gphoto2 --stdout --capture-movie | ffmpeg -hwaccel nvdec -c:v mjpeg_cuvid -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
