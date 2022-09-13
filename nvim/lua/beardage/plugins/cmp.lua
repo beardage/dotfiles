c2cd7a4bb247d5ee88ab2620f3aa5792909621b9
Kyle Beard <xypress@gmail.com>
2022-09-13 15:33:50 -0700

    feat: adding border to cmp menu

--- nvim/lua/beardage/plugins/cmp.lua
+++ nvim/lua/beardage/plugins/cmp.lua
@@ -24,6 +24,10 @@ cmp.setup({
 			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
 	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
