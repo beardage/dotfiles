ad0cd2edac6efae27f6a5b32fbb9d9a56984d186
Kyle Beard <xypress@gmail.com>
2022-09-21 16:41:18 -0700

    feat: switching to eslintd for formatting while using prettier plug

--- nvim/lua/beardage/plugins/null-ls.lua
+++ nvim/lua/beardage/plugins/null-ls.lua
@@ -16,7 +16,8 @@ require("null-ls").setup({
 	end,
 	sources = {
 		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.formatting.prettier,
+		require("null-ls").builtins.formatting.eslint_d,
 		require("null-ls").builtins.diagnostics.eslint_d,
+		require("null-ls").builtins.code_actions.eslint_d,
 	},
 })
