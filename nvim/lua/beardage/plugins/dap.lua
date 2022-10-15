ab20050badb00a15196af82ef7f5b6845bfb2cd6
Kyle Beard <xypress@gmail.com>
2022-10-15 13:21:52 -0700

    feat: adjusting dap shortcuts

--- nvim/lua/beardage/plugins/dap.lua
+++ nvim/lua/beardage/plugins/dap.lua
@@ -90,16 +90,16 @@ dap.configurations.typescriptreact = {
 	},
 }
 
-keymap("n", "<F5>", "[[<cmd>lua require('dap').continue()<CR>]]")
-keymap("n", "<F3>", "[[<cmd>lua require('dap').step_over()<CR>]]")
-keymap("n", "<F2>", "[[<cmd>lua require('dap').step_into()<CR>]]")
-keymap("n", "<F12>", "[[<cmd>lua require('dap').step_out()<CR>]]")
+keymap("n", "<leader>dc", "[[<cmd>lua require('dap').continue()<CR>]]")
+keymap("n", "<leader><Right>", "[[<cmd>lua require('dap').step_over()<CR>]]")
+keymap("n", "<leader><Down>", "[[<cmd>lua require('dap').step_into()<CR>]]")
+keymap("n", "<leader><Up>", "[[<cmd>lua require('dap').step_out()<CR>]]")
 keymap("n", "<leader>b", [[<cmd>lua require('dap').toggle_breakpoint()<CR>]])
 keymap("n", "<leader>B", [[<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>]])
 keymap("n", "<leader>lp", [[<cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>]])
 keymap("n", "<leader>dr", [[<cmd>lua require('dap').repl.open()<CR>]])
 keymap("n", "<leader>do", [[<cmd>lua require('dapui').open()<CR>]])
-keymap("n", "<leader>dc", [[<cmd>lua require('dapui').close()<CR>]])
+keymap("n", "<leader>dq", [[<cmd>lua require('dapui').close()<CR>]])
 
 dap.set_log_level("TRACE")
 
