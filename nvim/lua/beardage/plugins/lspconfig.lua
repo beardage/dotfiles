68caa9f932ab55d6596ebaf48456307956089e45
Kyle Beard <kyle@hihello.com>
2023-07-04 03:25:51 -0700

    feat: removing depricated lua lsp and tree options

--- nvim/lua/beardage/plugins/lspconfig.lua
+++ nvim/lua/beardage/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "sumneko_lua", "tsserver", "tailwindcss", "cssls", "html", "svelte", "jsonls" },
+	ensure_installed = { "lua_ls", "tsserver", "tailwindcss", "cssls", "html", "svelte", "jsonls" },
 })
 
 local lspconfig = require("lspconfig")
@@ -92,29 +92,29 @@ lspconfig.jsonls.setup({
 	on_attach = on_attach,
 })
 
-lspconfig.sumneko_lua.setup({
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-})
+lspconfig.lua_ls.setup {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 lspconfig.emmet_ls.setup({
 	capabilities = capabilities,
